34179f9ef0442723d09620dadb986ad5bc99336f
Clay Tran <huutrantoan@gmail.com>
2025-02-09 12:40:44 +0700

    feat: configure dayjs locale based on i18n settings

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,13 @@ import { App } from './App';
 import { AuthProvider } from './context/AuthContext.jsx';
 import './index.css';
 
+import dayjs from 'dayjs';
+import i18n from './i18n/config';
+
+const locale = i18n.language;
+
+dayjs.locale(locale);
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AuthProvider>
